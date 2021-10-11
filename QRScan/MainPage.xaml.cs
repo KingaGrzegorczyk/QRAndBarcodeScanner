791e3ec168bd81d76d47cfa2d54f19dd4547aef5
KingaGrzegorczyk <78357910+KingaGrzegorczyk@users.noreply.github.com>
2021-10-11 16:21:02 +0200

    Add project files.

---
+++ QRScan/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace QRScan
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ScanButtonClicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushAsync(scan);
+            scan.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    myCode.IsVisible = true;
+                    myCode.Text = result.Text;
+                });
+            };
+        }
+    }
+}
